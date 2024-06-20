029b2953e35489e4d376596e1ecbfa19f009064b
eduzissimo <eduardo.alves14@hotmail.com>
2024-06-20 20:32:15 -0300

    Initial commit: queue feito

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,16 +3,25 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self._items = []
+    # Método que retorna o tamanho da fila
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self._items)
+    # retorna se a fila está vazia
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self._items.append(value)
+    # adiciona elemento na fila
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if not self._items:
+            return None
+        return self._items.pop(0)
+    # remove o primeiro elemento da fila
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if index < 0 or index >= len(self._items):
+            raise IndexError("Índice Inválido ou Inexistente")
+        return self._items[index]
+    # busca
