0ef79b2d2513afcee78a9e03193681d238295616
eduzissimo <eduardo.alves14@hotmail.com>
2024-06-20 20:48:01 -0300

    Feat: file_management feito

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,16 @@
+import os
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not os.path.exists(path_file):
+        print(f'Arquivo {path_file} não encontrado', file=sys.stderr)
+        return None
+
+    if not path_file.endswith('.txt'):
+        print('Formato inválido', file=sys.stderr)
+        return None
+
+    with open(path_file, 'r', encoding='utf-8') as file:
+        lines = file.read().split('\n')
+    return lines
