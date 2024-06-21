ca67b833fb74630d151d7d0d86b283232bef116d
eduzissimo <eduardo.alves14@hotmail.com>
2024-06-20 22:27:45 -0300

    Feat: test_priority_queue

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,38 @@
-# from ting_file_management.priority_queue import PriorityQueue
+import pytest
+from ting_file_management.priority_queue import PriorityQueue
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    pq = PriorityQueue()
+    mock = [
+        {
+            "nome_do_arquivo": "arq1.txt",
+            "qtd_linhas": 6,
+            "linhas_do_arquivo": [""] * 6,
+        },
+        {
+            "nome_do_arquivo": "arq2.txt",
+            "qtd_linhas": 1,
+            "linhas_do_arquivo": [""] * 1,
+        },
+        {
+            "nome_do_arquivo": "arq3.txt",
+            "qtd_linhas": 3,
+            "linhas_do_arquivo": [""] * 3,
+        },
+    ]
+    expected_mock = [1, 3, 6]
+
+    for file in mock:
+        pq.enqueue(file)
+
+    assert len(pq) == 3
+
+    for item, expected_test in enumerate(expected_mock):
+        assert pq.search(item)["qtd_linhas"] == expected_test
+
+    assert pq.dequeue() == mock[1]
+    assert len(pq) == len(mock) - 1
+
+    with pytest.raises(IndexError):
+        pq.search(3)
